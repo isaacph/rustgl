78bafc753a5158dfe10b4f1d782fcac75ea142c5
isaacph <isaacphuffman@gmail.com>
2022-07-07 23:37:53 -0700

    Move remaining top level modules to their own folders

---
+++ src/networking/mod.rs
@@ -0,0 +1,34 @@
+use serde::{Serialize, Deserialize};
+use crate::SocketAddr;
+use std::fmt::Display;
+
+pub mod commands;
+pub mod client;
+pub mod server;
+pub mod tcp_buffering;
+pub mod config;
+pub mod common;
+pub mod example;
+
+#[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Copy, Clone)]
+pub enum Protocol {
+    TCP, UDP
+}
+
+impl Display for Protocol {
+    fn fmt(&self, out: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(out, "{:?}", self)
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+pub struct AddressPair {
+    pub tcp: SocketAddr,
+    pub udp: SocketAddr
+}
+
+impl Display for AddressPair {
+    fn fmt(&self, out: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(out, "{{udp: {}, tcp: {}}}", self.udp, self.tcp)
+    }
+}
